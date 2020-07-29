92c62a8cdc4d8a418581e588861429b450fbcd54
mitesh <mitesh@freshbits.in>
2020-07-29 11:08:00 +0530

    update in search features

---
+++ add_movie.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet"
+    href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+    integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+    crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css" rel="stylesheet">
+    <title>Add TO Your Movie List</title>
+</head>
+<body>
+    <div id="add-movie-box" class="border border-dark rounded col-lg-6 container p-4 my-4">
+        <h3 class="text-center">
+            Add Movie
+        </h3>
+        <form action="" method="post" class="col-12">
+            <label class="col-12 my-3">
+                Movie Name :
+                <input type="text" name="movie-name" id="movie-name" class="form-control">
+            </label>
+            <label class="col-12 my-3">
+                Released Date :
+                <input type="text" name="released_date" placeholder="yyyy-mm-dd" class="form-control">
+            </label>
+            <label class="col-12 my-3">
+                Download Links :
+                <input type="text" name="released_date" class="form-control">
+            </label>
+            <div class="col-12 my-3">
+                <input type="file" class="custom-file-input" id="#poster" name="poster" accept="image/*">
+                <label class="custom-file-label" for="poster">Poster</label>
+            </div>
+            <div class="col-12 text-center my-3">
+                <button type="button" class="btn btn-danger mt-3" onclick="window.location = 'my_movies.php'">
+                    <i class="fas fa-arrow-left"></i> Cancel
+                </button>
+                <button type="submit" class="btn btn-primary mt-3">
+                    Add Movie
+                </button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
